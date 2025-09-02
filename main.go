2df779dbeaced3902c2c16e9242fb5a622407fd8
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2025-09-03 00:21:49 +0200

    refactor: make code prettier

--- main.go
+++ main.go
@@ -3,18 +3,32 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
+	"time"
 
 	"github.com/klauspost/compress/zstd"
 	"github.com/siderolabs/talos/pkg/machinery/extensions"
 	"github.com/u-root/u-root/pkg/cpio"
 	"github.com/ulikunitz/xz"
 	"gopkg.in/yaml.v3"
+)
+
+const (
+	defaultTalosVersion    = "v1.10.6"
+	defaultFactoryBaseURL  = "https://factory.talos.dev"
+	defaultCacheDirName    = ".talos-schematic-id-to-raw-schematic-cache"
+	initramfsFilenameTmpl  = "%s-initramfs-amd64-%s.xz" // talosVersion, schematicID
+	initramfsURLTmpl       = "%s/image/%s/%s/initramfs-amd64.xz"
+	extensionsYAMLFileName = "extensions.yaml"
 )
 
 func main() {
@@ -24,133 +38,233 @@ func main() {
 }
 
 func run() error {
-	if len(os.Args) < 2 {
-		return fmt.Errorf("usage: %s <schematic-id>", os.Args[0])
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	cfg, err := buildConfig()
+	if err != nil {
+		return err
 	}
 
-	schematicID := os.Args[1]
-	talosVersion := "v1.10.6"
-	imageFactoryBaseURL := "https://factory.talos.dev"
+	if err = os.MkdirAll(cfg.CacheDir, 0o700); err != nil {
+		return fmt.Errorf("failed to create cache dir %q: %w", cfg.CacheDir, err)
+	}
+
+	localPath := cfg.cacheFilePath()
+	if err = ensureInitramfsCached(ctx, localPath, cfg.initramfsURL()); err != nil {
+		return err
+	}
+
+	initramfsFile, err := os.Open(localPath)
+	if err != nil {
+		return fmt.Errorf("failed to open %q: %w", localPath, err)
+	}
+
+	defer func() {
+		if closeErr := initramfsFile.Close(); closeErr != nil {
+			log.Printf("warning: failed to close file %q: %v", localPath, closeErr)
+		}
+	}()
+
+	raw, err := extractRawSchematic(bufio.NewReader(initramfsFile))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("raw schematic:\n%s", raw)
+	log.Printf("done, exiting")
+
+	return nil
+}
+
+type config struct {
+	SchematicID  string
+	TalosVersion string
+	BaseURL      string
+	CacheDir     string
+}
+
+func buildConfig() (*config, error) {
+	if len(os.Args) < 2 {
+		return nil, fmt.Errorf("usage: %s <schematic-id>", os.Args[0])
+	}
+
+	sid := os.Args[1]
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return fmt.Errorf("failed to get user home dir: %v", err)
+		return nil, fmt.Errorf("failed to get user home dir: %w", err)
 	}
 
-	cacheDir := filepath.Join(homeDir, ".talos-schematic-id-to-raw-schematic-cache")
-	if err = os.MkdirAll(cacheDir, 0o700); err != nil {
-		return fmt.Errorf("failed to create cache dir %q: %v", cacheDir, err)
+	return &config{
+		SchematicID:  sid,
+		TalosVersion: defaultTalosVersion,
+		BaseURL:      defaultFactoryBaseURL,
+		CacheDir:     filepath.Join(homeDir, defaultCacheDirName),
+	}, nil
+}
+
+func (c *config) cacheFilePath() string {
+	return filepath.Join(
+		c.CacheDir,
+		fmt.Sprintf(initramfsFilenameTmpl, c.TalosVersion, c.SchematicID),
+	)
+}
+
+func (c *config) initramfsURL() string {
+	return fmt.Sprintf(initramfsURLTmpl, c.BaseURL, c.SchematicID, c.TalosVersion)
+}
+
+//nolint:cyclop
+func ensureInitramfsCached(ctx context.Context, path, url string) error {
+	if _, err := os.Stat(path); err == nil {
+		log.Printf("using cached initramfs at %q", path)
+
+		return nil
 	}
 
-	filePath := filepath.Join(cacheDir, fmt.Sprintf("%s-initramfs-amd64-%s.xz", talosVersion, schematicID))
+	if err := os.MkdirAll(filepath.Dir(path), 0o700); err != nil {
+		return fmt.Errorf("mkdir: %w", err)
+	}
 
-	_, err = os.Stat(filePath)
+	tmp, err := os.CreateTemp(filepath.Dir(path), ".partial-*")
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return fmt.Errorf("failed to stat file %q: %v", filePath, err)
-		}
-
-		// download the file
-		initramfsURL := fmt.Sprintf("%s/image/%s/%s/initramfs-amd64.xz", imageFactoryBaseURL, schematicID, talosVersion)
-
-		log.Printf("downloading initramfs from %q to %q", initramfsURL, filePath)
-
-		out, err := os.Create(filePath)
-		if err != nil {
-			return fmt.Errorf("failed to create file %q: %v", filePath, err)
-		}
-
-		defer out.Close()
-
-		resp, err := http.Get(initramfsURL)
-		if err != nil {
-			return fmt.Errorf("failed to download initramfs from %q: %v", initramfsURL, err)
-		}
-
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return fmt.Errorf("failed to download initramfs from %q: status code %d", initramfsURL, resp.StatusCode)
-		}
-
-		if _, err = io.Copy(out, resp.Body); err != nil {
-			return fmt.Errorf("failed to write initramfs to %q: %v", filePath, err)
-		}
-	} else {
-		log.Printf("using cached initramfs at %q", filePath)
+		return fmt.Errorf("create temp: %w", err)
 	}
 
-	f, err := os.Open(filePath)
+	defer func() { _ = os.Remove(tmp.Name()); _ = tmp.Close() }()
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return fmt.Errorf("failed to open %q: %v", filePath, err)
+		return fmt.Errorf("new request: %w", err)
 	}
 
-	in := bufio.NewReader(f)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("download: %w", err)
+	}
 
-	r := &discarder{r: in}
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			log.Printf("warning: failed to close response body: %v", closeErr)
+		}
+	}()
 
-	cpioR := cpio.Newc.Reader(r)
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("download status %d", resp.StatusCode)
+	}
+
+	if _, err = io.Copy(tmp, resp.Body); err != nil {
+		return fmt.Errorf("write: %w", err)
+	}
+
+	if err = tmp.Sync(); err != nil {
+		return fmt.Errorf("fsync: %w", err)
+	}
+
+	if err = tmp.Close(); err != nil {
+		return fmt.Errorf("close tmp: %w", err)
+	}
+
+	return os.Rename(tmp.Name(), path)
+}
+
+func extractRawSchematic(reader *bufio.Reader) (string, error) {
+	var closeFuncs []func()
+
+	defer func() {
+		for _, c := range closeFuncs {
+			c()
+		}
+	}()
 
 	for {
-		magic, err := in.Peek(4)
+		decReader, closeFunc, err := decompressingReadCloser(reader)
 		if err != nil {
-			return err
+			return "", err
 		}
 
-		var rdr io.Reader = in
+		closeFuncs = append(closeFuncs, closeFunc)
 
-		switch {
-		case bytes.Equal(magic, []byte{0xfd, '7', 'z', 'X'}):
-			// xz-compressed
-			if rdr, err = xz.NewReader(in); err != nil {
-				return err
-			}
-		case bytes.Equal(magic, []byte{0x28, 0xb5, 0x2f, 0xfd}):
-			// zstd-compressed
-			if rdr, err = zstd.NewReader(in); err != nil {
-				return err
-			}
-		}
+		reader = bufio.NewReader(decReader)
 
-		in = bufio.NewReader(rdr)
+		d := &discarder{r: reader}
+		cpioReader := cpio.Newc.Reader(d)
 
-		r = &discarder{r: in}
-		cpioR = cpio.Newc.Reader(r)
+		var rec cpio.Record
 
 		for {
-			record, err := cpioR.ReadRecord()
-			if err == io.EOF {
-				break
-			}
-
-			if err != nil {
-				return err
-			}
-
-			log.Printf("found record: %q", record.Name)
-
-			if record.Name == "extensions.yaml" {
-				var extensionConfig extensions.Config
-
-				yamlReader := record.ReaderAt.(*io.SectionReader)
-
-				if err = yaml.NewDecoder(yamlReader).Decode(&extensionConfig); err != nil {
-					return err
+			if rec, err = cpioReader.ReadRecord(); err != nil {
+				if errors.Is(err, io.EOF) {
+					break
 				}
 
-				lastLayer := extensionConfig.Layers[len(extensionConfig.Layers)-1]
+				return "", err
+			}
 
-				log.Printf("raw schematic:\n%s", lastLayer.Metadata.ExtraInfo)
-				log.Printf("done, exiting")
+			log.Printf("found record: %q", rec.Name)
 
-				return nil
+			if rec.Name == extensionsYAMLFileName {
+				return parseRawFromExtensions(rec.ReaderAt)
 			}
 		}
 
-		if err = eatPadding(in); err != nil {
-			return err
+		if err = eatPadding(reader); err != nil {
+			return "", err
 		}
 	}
+}
+
+func decompressingReadCloser(in *bufio.Reader) (rdr io.Reader, closeFunc func(), err error) {
+	magic, err := in.Peek(4)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	switch {
+	case bytes.Equal(magic, []byte{0xfd, '7', 'z', 'X'}): // xz
+		var reader io.Reader
+
+		if reader, err = xz.NewReader(in); err != nil {
+			return nil, nil, err
+		}
+
+		return reader, func() {}, nil
+	case bytes.Equal(magic, []byte{0x28, 0xb5, 0x2f, 0xfd}): // zstd
+		var decoder *zstd.Decoder
+
+		if decoder, err = zstd.NewReader(in); err != nil {
+			return nil, nil, err
+		}
+
+		return decoder, decoder.Close, nil
+	default:
+		return in, func() {}, nil // return the original reader
+	}
+}
+
+func parseRawFromExtensions(readerAt io.ReaderAt) (string, error) {
+	sectionReader, ok := readerAt.(*io.SectionReader)
+	if !ok {
+		return "", fmt.Errorf("unexpected ReaderAt type %T; want *io.SectionReader", readerAt)
+	}
+
+	var extensionsConfig extensions.Config
+
+	if err := yaml.NewDecoder(sectionReader).Decode(&extensionsConfig); err != nil {
+		return "", err
+	}
+
+	if len(extensionsConfig.Layers) == 0 {
+		return "", errors.New("extensions.yaml has no layers")
+	}
+
+	last := extensionsConfig.Layers[len(extensionsConfig.Layers)-1]
+
+	return last.Metadata.ExtraInfo, nil
 }
 
 // discarder is used to implement ReadAt from a Reader
@@ -166,7 +280,7 @@ type discarder struct {
 // It is an error for the offset to be negative.
 func (r *discarder) ReadAt(p []byte, off int64) (int, error) {
 	if off-r.pos < 0 {
-		return 0, fmt.Errorf("negative seek on discarder not allowed")
+		return 0, errors.New("negative seek on discarder not allowed")
 	}
 
 	if off != r.pos {
@@ -193,20 +307,16 @@ var _ io.ReaderAt = &discarder{}
 func eatPadding(in io.ByteScanner) error {
 	for {
 		b, err := in.ReadByte()
-		if err == io.EOF {
-			return nil
-		}
-
 		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+
 			return err
 		}
 
 		if b != 0 {
-			if err = in.UnreadByte(); err != nil {
-				return err
-			}
-
-			return nil
+			return in.UnreadByte()
 		}
 	}
 }
